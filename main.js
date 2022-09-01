3866148e893ba27caa68aee2093122dda07af64f
salah301 <99408018+salah301@users.noreply.github.com>
2022-09-01 20:26:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+let  catrona=''
+for(let i = 0 ; i > -1 ; i++){
+
+    catrona +=`<h1>hello</h1> `
+    console.log("hello");
+}
+document.getElementById("demo").innerHTML = catrona
+
+
+
+let  catrona1 = ""
+for(let i = 0 ; i > -1 ; i++){
+
+    catrona1 +=`<h1>hello</h1> `
+    console.log("hello");
+}
+document.getElementById("demo1").innerHTML = catrona1
+
+let  catrona2 = ""
+for(let i = 0 ; i > -1 ; i++){
+
+    catrona2 +=`<h1>hello</h1> `
+    console.log("hello");
+}
+document.getElementById("demo2").innerHTML = catrona2
+let  catrona3 = ""
+for(let i = 0 ; i > -1 ; i++){
+
+    catrona3 +=`<h1>hello</h1> `
+    console.log("hello");
+}
+document.getElementById("demo3").innerHTML = catrona3
+let  catrona4 = ""
+for(let i = 0 ; i > -1 ; i++){
+
+    catrona4 +=`<h1>hello</h1> `
+    console.log("hello");
+}
+document.getElementById("demo4").innerHTML = catrona4
+
+let  catrona5 = ""
+for(let i = 0 ; i > -1 ; i++){
+
+    catrona5 +=`<h1>hello</h1> `
+    console.log("hello");
+}
+document.getElementById("demo5").innerHTML = catrona5
+
+ 
+ 
